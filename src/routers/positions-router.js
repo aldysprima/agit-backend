6063618c9e5232c88798a823f8b7a45cffbc162a
aldysprima <aldys.prima@gmail.com>
2023-02-23 19:46:43 +0700

    adding middleware for accesing positions api

--- src/routers/positions-router.js
+++ src/routers/positions-router.js
@@ -1,8 +1,9 @@
 const routers = require("express").Router();
+const authMiddleware = require("../helpers/userAuthMiddleware");
 
 const { positions } = require("../controllers");
 
-routers.get("/positions", positions.getPositions);
-routers.get("/position/:id", positions.getPositionById);
+routers.get("/positions", authMiddleware, positions.getPositions);
+routers.get("/position/:id", authMiddleware, positions.getPositionById);
 
 module.exports = routers;
