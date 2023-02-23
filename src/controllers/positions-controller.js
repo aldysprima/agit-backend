711451791b819c116adbc10a12ed318e997514b5
aldysprima <aldys.prima@gmail.com>
2023-02-24 00:15:25 +0700

    add paging and search functionality

--- src/controllers/positions-controller.js
+++ src/controllers/positions-controller.js
@@ -2,9 +2,24 @@ const database = require("../config").promise();
 
 // Get All Joblist
 module.exports.getPositions = async (req, res) => {
+  // Capture Query Params
+  const isFullTime = req.query.full_time || false;
+  const description = req.query.description || "";
+  const location = req.query.location || "";
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 5;
+  const offset = (page - 1) * limit;
+  let type;
+  if (isFullTime === "true") {
+    type = "Full Time";
+  }
   try {
-    //Execute Query in DB
-    const GET_POSITIONS = `select * from positions`;
+    if (type === "Full Time") {
+      const GET_POSITIONS = `select * from positions where location LIKE '%${location}%' and description LIKE '%${description}%' and type LIKE '%${type}%' `;
+      const [POSITIONS] = await database.execute(GET_POSITIONS);
+      return res.status(200).send(POSITIONS);
+    }
+    const GET_POSITIONS = `select * from positions where location LIKE '%${location}%' and description LIKE '%${description}%' LIMIT ${offset}, ${limit} ;`;
     const [POSITIONS] = await database.execute(GET_POSITIONS);
     //Send Response
     return res.status(200).send(POSITIONS);
